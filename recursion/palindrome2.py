b2a2f6ea20a2e3ca79b2d46d2483181855d64e26
Deepash-s <deepashs18@gmail.com>
2025-08-04 13:35:56 +0530

    add palindrome optimal solution

---
+++ recursion/palindrome2.py
@@ -0,0 +1,8 @@
+#A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and 
+# removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric 
+# characters include letters and numbers.
+
+s = input('Enter a string: ')
+palindrome = ''.join(char.lower() for char in s if char.isalnum())
+
+print(palindrome == palindrome[::-1])
