60246a8f0c92be1df454e57bd90766db03d1f0bb
Deepash-s <deepashs18@gmail.com>
2025-09-28 22:34:58 +0530

    add insertion sort prblm

---
+++ sorting/insertion.py
@@ -0,0 +1,10 @@
+arr = [6, 5, 4, 3, 2, 1]
+
+for i in range(len(arr)):
+    j = i
+    while ( j>0 and arr[j-1]>arr[j]):
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+        j -= 1
+        
+
+print(arr)
