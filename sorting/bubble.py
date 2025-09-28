ad8347190ad6ca9b06e01de49fa5f504e227b071
Deepash-s <deepashs18@gmail.com>
2025-09-28 17:48:14 +0530

    add bubble sort prblm

---
+++ sorting/bubble.py
@@ -0,0 +1,10 @@
+arr = [13, 46, 24, 52, 20, 9]
+n = len(arr)
+
+
+for i in range(n-1):
+    for j in range(n-i-1):
+        if (arr[j] > arr[j+1]):
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+    
+print(arr)
