bd2bb76710e6f81eb81773f685814761f50c3efe
Deepash-s <deepashs18@gmail.com>
2025-10-05 00:28:44 +0530

    add changes

--- Arrays/isArraySorted.py
+++ Arrays/isArraySorted.py
@@ -3,9 +3,7 @@ arr2 = [1, 2, 3, 4, 5, 6]
 
 def isArraySorted(arr1):
     for i in range(1, len(arr1)):
-        if(arr1[i]>=arr1[i-1]):
-            pass
-        else:
+        if(arr1[i]<arr1[i-1]):
             return False
     return True
         
