02ecf10c7ba2e5d0887ec720a00d3aad4ab9ceb0
Deepash-s <deepashs18@gmail.com>
2025-10-18 21:17:26 +0530

    missing number brute

---
+++ Arrays/Easy/missingNo1.py
@@ -0,0 +1,13 @@
+n = 5
+arr = [1, 2, 3, 4]
+
+for i in range(1, n+1):
+    flag = 0
+    for j in range(len(arr)):
+        if arr[j] == i:
+            flag = 1
+            break
+    if flag == 0:
+        print(i) 
+        break
+        
