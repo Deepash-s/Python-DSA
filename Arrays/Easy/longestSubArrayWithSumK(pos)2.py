56aaa142cd748d03b436889b6ee546eaba56ab34
Deepash-s <deepashs18@gmail.com>
2025-10-26 09:36:50 +0530

    longest sub array with sum K(brute)

---
+++ Arrays/Easy/longestSubArrayWithSumK(pos)2.py
@@ -0,0 +1,16 @@
+#brute
+
+arr = [1, 2, 3, 1, 1, 1, 1, 4, 2, 3]
+n = len(arr)
+k = 3
+length = 0
+
+for i in range(n):
+    sums = 0
+    for j in range(i, n):
+        sums += arr[j]
+    
+        if sums == k:
+            length = max(length, j-i+1)
+        
+print(length)
