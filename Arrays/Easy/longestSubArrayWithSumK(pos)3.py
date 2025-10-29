832ddda7a1d91e1cc2ec7a788280d45e0ae552de
Deepash-s <deepashs18@gmail.com>
2025-10-29 18:17:46 +0530

    longest Sub Array With Sum K(better)

---
+++ Arrays/Easy/longestSubArrayWithSumK(pos)3.py
@@ -0,0 +1,26 @@
+#Better
+
+arr = [1, 2, 3, 1, 1, 1, 1, 4, 2, 3]
+n = len(arr)
+k = 3
+preSumMap = {}
+maxLen = 0
+sums = 0
+
+for i in range(n):
+    sums += arr[i]
+    
+    if sums == k:
+        maxLen = max(maxLen, i+1)
+        
+    rem = sums - k
+    
+    if rem in preSumMap:
+        length = i - preSumMap[rem]
+        maxLen = max(maxLen, length)
+        
+    if sums not in preSumMap:
+        preSumMap[sums] = i
+
+
+print(maxLen)
