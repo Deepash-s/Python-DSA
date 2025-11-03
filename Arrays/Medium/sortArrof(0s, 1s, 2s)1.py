68743a371ebf7cc24dcb1968263b39fe100dc48d
Deepash-s <deepashs18@gmail.com>
2025-11-03 22:50:16 +0530

    sort array of 0s, 1s, 2s

---
+++ Arrays/Medium/sortArrof(0s, 1s, 2s)1.py
@@ -0,0 +1,7 @@
+#brute (sorting using merge/quick)
+
+arr = [2, 0, 2, 1, 1, 0]
+n = len(arr)
+
+arr.sort()
+print(arr)
---
+++ Arrays/Medium/sortArrof(0s, 1s, 2s)2.py
@@ -0,0 +1,24 @@
+arr = [2, 0, 2, 1, 1, 0]
+n = len(arr)
+cnt0, cnt1, cnt2 = 0, 0, 0
+
+for i in range(n):
+    if arr[i] == 0:
+        cnt0 += 1
+    elif arr[i] == 1:
+        cnt1 += 1
+    else:
+        cnt2 += 1
+        
+        
+for i in range(cnt0):
+    arr[i] = 0
+    
+for i in range(cnt0, cnt0 + cnt1):
+    arr[i] = 1
+    
+for i in range(cnt0 + cnt1, n):
+    arr[i] = 2
+    
+    
+print(arr)
